ce894e00b23cfd5b196e5a40cfe2b5830369b831
famouschamus <chamusg@gmail.com>
2018-02-01 14:49:38 -0500

    initial commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Lab9
+{
+    class Circle
+    {
+
+        public Circle(double radius)
+        {
+
+            double area = CalculateArea(radius);
+            double circumference = CalculateCircumference(radius);
+            Console.WriteLine("The area of your circle is " + area + ". The circumference of your circle is " + circumference);
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            double area = Math.PI * (radius * radius);
+            area = FormatArea(area);
+            return area;
+        }
+        public static double CalculateCircumference(double radius)
+        {
+            double circumference = (radius + radius) * Math.PI;
+            circumference = FormatCircumference(circumference);
+            return circumference;
+        }
+        public static double FormatArea(double a)
+        {
+
+            a = Math.Round(a, 2);
+
+            return a;
+        }
+        public static double FormatCircumference(double c)
+        {
+
+            c = Math.Round(c, 2);
+
+            return c;
+        }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main()
+        {
+            double radius = 0;
+            bool continuey = true;
+            List<Circle> allTheCicles = new List<Circle>();
+            while (continuey == true)
+            {
+                Console.WriteLine("Hello, Please give me the radius of a Circle you'd like to create");
+                string radiusInput = Console.ReadLine();
+                bool valid = double.TryParse(radiusInput, out radius);
+                if (valid == false || radius <= 0)
+                {
+                    Console.WriteLine("This is not a valid input for radius");
+                    Main();
+                }
+
+                Circle circle = new Circle(radius);
+                allTheCicles.Add(circle);
+
+                Console.WriteLine("Would you like to continue? y/n");
+                string moreCircles = Console.ReadLine().ToLower();
+                if (moreCircles == "y" || moreCircles == "yes")
+                {
+
+                }
+                else if (moreCircles == "n" || moreCircles == "no")
+                {
+                    Console.WriteLine("Thanks, have a nice day! You made this many circles:");
+                    Console.WriteLine(allTheCicles.Count);
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("I do not understand, terminating program");
+                    break;
+                }
+            }
+
+        }
+    }
+}
